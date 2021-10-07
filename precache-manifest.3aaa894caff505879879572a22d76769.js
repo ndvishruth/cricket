4847b052b56307fef3593db3c9f42ca3c0c15b82
ndvishruth <ndvishruth@gmail.com>
2021-10-07 13:19:48 +0530

    Updates

---
+++ precache-manifest.3aaa894caff505879879572a22d76769.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399f54c0775ca85da0c75be18081b99f",
+    "url": "/cricket/index.html"
+  },
+  {
+    "revision": "cc5fc6711a02b9a3cf69",
+    "url": "/cricket/static/css/main.b5931c77.chunk.css"
+  },
+  {
+    "revision": "b230f828ced208b38fdc",
+    "url": "/cricket/static/js/2.10fc4d48.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/cricket/static/js/2.10fc4d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5fc6711a02b9a3cf69",
+    "url": "/cricket/static/js/main.84b25565.chunk.js"
+  },
+  {
+    "revision": "ca8b2cf0d0031d7c3eda",
+    "url": "/cricket/static/js/runtime-main.954fc421.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/cricket/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/cricket/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/cricket/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "4a436785f188547b8268507a25f4e9ae",
+    "url": "/cricket/static/media/landing-bg.4a436785.jpg"
+  }
+]);
